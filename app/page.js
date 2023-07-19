d2e31dcacc6e8b5acf037a6a94383bb14c294254
Isaac Idibia <idibiaisaac3@gmail.com>
2023-07-19 12:31:36 +0100

    nav

--- app/page.js
+++ app/page.js
@@ -1,3 +1,5 @@
+import Feed from "@/components/Feed";
+
 export default function Home() {
   return (
     <section className="w-full flex-center flex-col">
@@ -11,6 +13,7 @@ export default function Home() {
       </p>
 
       {/* Feed */}
+      <Feed />
     </section>
   );
 }
